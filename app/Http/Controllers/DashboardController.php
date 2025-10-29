9f9c2480a0dccec57ec103eb3b928170c81036de
Salahudin-cloud <ayubi54321@gmail.com>
2025-10-29 23:47:24 +0700

    feat: add chart student on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class DashboardController extends Controller
 {
     public function index(): view
     {
-        return view('dashboard');
+        $enrollmentData = Student::select('enrollment_year', DB::raw('COUNT(*) as total'))
+            ->groupBy('enrollment_year')
+            ->orderBy('enrollment_year', 'asc')
+            ->get();
+
+        $labels = $enrollmentData->pluck('enrollment_year');
+        $data = $enrollmentData->pluck('total');
+        return view('dashboard', compact('labels', 'data'));
     }
 }
